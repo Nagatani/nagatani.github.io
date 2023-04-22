8d91f71c5d3bf53bb04f88a9a4a5ab07daf7c401
Nagatani <Nagatani@users.noreply.github.com>
2023-04-22 13:50:30 +0900

    fix: sortorderを日付順に変更するのと、投稿日時📅の書式変更

--- components/date-formatter.tsx
+++ components/date-formatter.tsx
@@ -7,7 +7,7 @@ type Props = {
 const DateFormatter = ({ dateString }: Props) => {
   if (dateString) {
     const date = parseISO(dateString)
-    return <time dateTime={dateString}>{format(date, 'LLLL	d, yyyy')}</time>
+    return <time dateTime={dateString}>{format(date, 'yyyy/MM/dd HH:mm')}</time>
   }
   return <></>
 }
--- components/more-stories.tsx
+++ components/more-stories.tsx
@@ -4,11 +4,16 @@ import Post from '../types/post'
 type Props = {
   posts: Post[]
 }
+const sortInfoByDate = (a: Post, b: Post) => {
+  const adate = new Date(a.date);
+  const bdate = new Date(b.date);
+  return adate < bdate ? 1 : -1;
+};
 
 const MoreStories = ({ posts }: Props) => {
   return (
     <section>
-      {posts.map((post) => (
+      {posts.sort((a, b) => sortInfoByDate(a,b)).map((post) => (
         <PostPreview
           key={post.slug}
           title={post.title}
