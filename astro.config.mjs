0806ae4f6a12e0937c75adf48db0ebb28afd09b8
Nagatani <nagatani@work-ark.jp>
2026-01-01 05:26:27 +0900

    リンクのrelをそれっぽくいじったりする

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,34 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
+import rehypeExternalLinks from 'rehype-external-links';
+
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://nagatani.github.io',
-	integrations: [mdx(), sitemap()],
+	markdown: {
+		rehypePlugins: [
+			[
+				rehypeExternalLinks,
+				{
+					target: '_blank',
+					rel: ['nofollow', 'noopener', 'noreferrer'],
+				},
+			],
+		],
+	},
+	integrations: [
+		mdx({
+			rehypePlugins: [
+				[
+					rehypeExternalLinks,
+					{
+						target: '_blank',
+						rel: ['nofollow', 'noopener', 'noreferrer'],
+					},
+				],
+			],
+		}),
+		sitemap(),
+	],
 });
